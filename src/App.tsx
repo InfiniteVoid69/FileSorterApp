1bbf8abeebf7fc94eae0f3edaaae9c5322a03f95
InfiniteVoid <ADJordan305@hotmail.com>
2025-04-08 13:51:08 -0500

    pannel layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import "./global.css";
-
-
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 function App() {
-  return <></>;
+  return (
+    <div className="h-screen">
+      <PanelGroup
+        autoSaveId="test"
+        direction="horizontal"
+        className="h-full"
+      >
+        <Panel defaultSize={25}>
+          <div className="h-full p-4 rounded-lg border-2 border-blue-500 bg-zinc-900 text-white font-bold overflow-auto">
+            Directories
+          </div>
+        </Panel>
+        <PanelResizeHandle className="w-[6px] bg-zinc-800 cursor-col-resize" />
+        <Panel>
+          <div className="h-full p-4 rounded-lg border-2 border-blue-500 bg-zinc-900 text-white font-bold overflow-auto">
+            Files
+          </div>
+        </Panel>
+        <PanelResizeHandle className="w-[6px] bg-zinc-800 cursor-col-resize" />
+        <Panel defaultSize={25}>
+          <div className="h-full p-4 rounded-lg border-2 border-blue-500 bg-zinc-900 text-white font-bold overflow-auto">
+            Sorting Rules
+          </div>
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
 }
 
 export default App;
