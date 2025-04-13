e4a973a88505e39d7eb53959022fd4b9bb03ed40
InfiniteVoid <ADJordan305@hotmail.com>
2025-04-13 02:36:52 -0500

    layout

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+
+export const DraggableTopBar = () => {
+  return <header className="absolute inset-0 h-8 bg-transparent" />
+}
+
+interface Props {
+  children?: ReactNode;
+}
+
+export const SideBar = ({children}: Props ) => {
+  return (
+    <div className="h-full p-2 mt-8 text-white overflow-auto">
+      {children}
+    </div>
+  );
+};
+
+export const FileDisplay = ({children}: Props ) => {
+  return (
+    <div className="h-full p-2 bg-zinc-800 text-white overflow-auto">
+      FileDisplay
+      {children}
+    </div>
+  );
+};
